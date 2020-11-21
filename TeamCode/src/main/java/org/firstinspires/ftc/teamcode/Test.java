5ff7c21a16c0ef1b2c933e3a1d914d044493ecff
austindeng <austin12709@gmail.com>
2020-11-21 11:18:40 -0600

    TeleOp update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Test extends OpMode {
+    DcMotor fl;
+    DcMotor fr;
+    DcMotor bl;
+    DcMotor br;
+    @Override
+    public void init() {
+        fl = hardwareMap.dcMotor.get("fl");
+        fr = hardwareMap.dcMotor.get("fr");
+        bl = hardwareMap.dcMotor.get("bl");
+        br = hardwareMap.dcMotor.get("br");
+
+    }
+
+    @Override
+    public void loop() {
+        if(Math.abs(gamepad1.left_stick_y) > 0.1 || Math.abs(gamepad1.left_stick_x) > 0.1|| Math.abs(gamepad1.right_stick_x) > 0.1){
+            double FLP = gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x;
+            double FRP = -gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x;
+            double BLP = gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x;
+            double BRP = -gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x;
+
+            double max = Math.max(Math.max(FLP, FRP), Math.max(BLP, BRP));
+
+            if(max > 1) {
+                FLP /= max;
+                FRP /= max;
+                BLP /= max;
+                BRP /= max;
+            }
+
+            if(gamepad1.right_trigger > 0.1){
+                fl.setPower(FLP * 0.35);
+                fr.setPower(FRP * 0.35);
+                bl.setPower(BLP * 0.35);
+                br.setPower(BRP * 0.35);
+            }
+            else {
+                fl.setPower(FLP);
+                fr.setPower(FRP);
+                br.setPower(BRP);
+                bl.setPower(BLP);
+            }
+
+        }
+
+    }
+}
